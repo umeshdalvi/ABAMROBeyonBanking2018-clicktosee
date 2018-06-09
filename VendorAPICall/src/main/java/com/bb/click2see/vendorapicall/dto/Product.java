5c6e2c33e8027ad73b6323f40ef26ef329a1f6ec
C34795 <umesh.dalvi@nl.abnamro.com>
2018-06-09 06:00:33 +0200

    working sample

--- VendorAPICall/src/main/java/com/bb/click2see/vendorapicall/dto/Product.java
+++ VendorAPICall/src/main/java/com/bb/click2see/vendorapicall/dto/Product.java
@@ -3,26 +3,84 @@
  */
 package com.bb.click2see.vendorapicall.dto;
 
-import lombok.Data;
-
-@Data
 public class Product {
+	
+	private final int id;
 
-	public Product(long incrementAndGet, int format) {
-		
+	private final String name;
+	
+	public Product(int id, String name) {
+		this.id =id;
+		this.name =name;
 	}
-	
-	private int id;
 
-	private String name;
 	
-	private Double price;
 	
-	private String description;
+	/*private Double price = 0d;
 	
-	private String offers;
+	private String description = null;
 	
-	private boolean isLoanAvailable;
+	private String offers = null;
 	
-	private String loanOffers;
+	private boolean isLoanAvailable = false;
+	
+	private String loanOffers = null;*/
+
+	public int getId() {
+		return id;
+	}
+
+	/*public void setId(int id) {
+		this.id = id;
+	}
+*/
+	public String getName() {
+		return name;
+	}
+
+/*	public void setName(String name) {
+		this.name = name;
+	}*/
+
+	/*public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getOffers() {
+		return offers;
+	}
+
+	public void setOffers(String offers) {
+		this.offers = offers;
+	}
+
+	public boolean isLoanAvailable() {
+		return isLoanAvailable;
+	}
+
+	public void setLoanAvailable(boolean isLoanAvailable) {
+		this.isLoanAvailable = isLoanAvailable;
+	}
+
+	public String getLoanOffers() {
+		return loanOffers;
+	}
+
+	public void setLoanOffers(String loanOffers) {
+		this.loanOffers = loanOffers;
+	}
+	*/
+	
 }
