7eaea831be4141c2568fcca14136f9f59cc1df6a
C38717 <C38717@NLLR4000511504.solon.prd>
2018-06-09 11:53:38 +0200

    chnages

---
+++ VisionAPICall/src/main/java/com/bb/click2see/visionapicall/VisionAPICallApplication.java
@@ -0,0 +1,13 @@
+package com.bb.click2see.visionapicall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionAPICallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionAPICallApplication.class, args);
+	}
+
+}
